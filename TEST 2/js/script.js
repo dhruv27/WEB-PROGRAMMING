7fd805310c5e99fb3058c2923ed1400a14cc7715
dhruv27 <dhruv27.dk@gmail.com>
2017-03-24 10:19:15 +0530

    Update script.js

--- TEST 2/js/script.js
+++ TEST 2/js/script.js
@@ -2,27 +2,22 @@
 // console.log(document.getElementById("title"));
 // console.log(document instanceof HTMLDocument);
 
-function Name () {
-  var name =
-   document.getElementById("name").value;
-   var message = "<h2>Hello " + name + "!</h2>";
+// function Name () {
+//   var name =
+//    document.getElementById("name").value;
+//    var message = "<h2>Hello " + name + "!</h2>";
 
-  // document
-  //   .getElementById("content")
-  //   .textContent = message;
+//   // document
+//   //   .getElementById("content")
+//   //   .textContent = message;
 
-  document
-    .getElementById("content")
-    .innerHTML = message;
+//   document
+//     .getElementById("content")
+//     .innerHTML = message;
 
-  if (name === "student") {
-    var title = 
-      document
-        .querySelector("#title")
-        .textContent;
-    title += " & Lovin' it!";
-    document
-        .querySelector("h1")
-        .textContent = title;
+  if (name === "DHRUV") {
+ function myFunction() {
+    var x = document.getElementById("myTable").rows[0].cells.length;
+    document.getElementById("demo").innerHTML = "Found " + x + " cells in the first tr element.";
   }
 }
