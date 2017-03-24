f41bf844689067a7b8a63134af05f6eb7b94f3bd
dhruv27 <dhruv27.dk@gmail.com>
2017-03-24 10:29:09 +0530

    Update script.js

--- TEST 2/js/script.js
+++ TEST 2/js/script.js
@@ -14,10 +14,24 @@
 //   document
 //     .getElementById("content")
 //     .innerHTML = message;
-function myFunction() {
-  if (name === "DHRUV") {
+// function myFunction() {
+//   if (name === "DHRUV") {
  
-    var x = document.getElementById("myTable").rows[0].cells.length;
+//     var x = document.getElementById("myTable").rows[0].cells.length;
    
-  }
+//   }
+// }
+function myFunction() {
+    var x = document.createElement("TABLE");
+    x.setAttribute("id", "myTable");
+    document.body.appendChild(x);
+
+    var y = document.createElement("TR");
+    y.setAttribute("id", "myTr");
+    document.getElementById("myTable").appendChild(y);
+
+    var z = document.createElement("TD");
+    var t = document.createTextNode("cell");
+    z.appendChild(t);
+    document.getElementById("myTr").appendChild(z);
 }
